f47a606ce4d16a449941b428644bc6c89a00339d
MEIE\38308275211 <gunnar.rohtsalu@partner.smit.ee>
2017-11-16 23:55:04 +0200

    'stuff'

--- 12f_app/src/main/java/com/gitlab/rohtsalu/app/resources/StockItemResource.java
+++ 12f_app/src/main/java/com/gitlab/rohtsalu/app/resources/StockItemResource.java
@@ -2,6 +2,8 @@ package com.gitlab.rohtsalu.app.resources;
 
 import com.gitlab.rohtsalu.app.domain.model.StockItem;
 import com.gitlab.rohtsalu.app.respository.StockItemRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -19,16 +21,21 @@ public class StockItemResource {
     @Autowired
     private StockItemRepository stockItemRepository;
 
+    private static final Logger logger = LoggerFactory.getLogger(StockResource.class);
+
     @RequestMapping(method = RequestMethod.GET, value = "/{id}")
     public StockItem stockItem(@PathVariable("id") Long id, HttpServletResponse response) {
+        logger.info("Starting search of stock item(id={}) search", id);
 
         StockItem stockItem = stockItemRepository.findOne(id);
 
         if (stockItem == null) {
+            logger.info("Stock item(id={}) has not been found", id);
             response.setStatus(HttpStatus.NOT_FOUND.value());
             return null;
         }
 
+        logger.info("Finishing search of stock item(id={}) search", id);
         return stockItem;
     }
 
